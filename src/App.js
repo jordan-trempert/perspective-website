305d3625c516841d63adca4440698937950e700a
Jordan Trempert <32543343+jordan-trempert@users.noreply.github.com>
2022-01-18 15:29:40 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -72,9 +72,9 @@ function Home() {
             <br />
             <span id="dscontent">
               Project Paradox (Beta)<br />
-              <a href="https://github.com/jordan-trempert/perspective-website/blob/main/Perspective%20Part%202%20(Beta%201).exe?raw=true">
+              <a href="https://drive.google.com/file/d/19GtTaOrxCL1h7xOxKLSwUCLskxLHauSP/view?usp=sharing">
                 Windows
-              </a> | <a href="https://github.com/jordan-trempert/perspective-website/blob/main/Perspective%20Part%202%20(Beta%201).apk?raw=true"> Android</a>
+              </a> | <a href="https://drive.google.com/file/d/13A2-_uqeSsBBTo_mN718pn2Tl7sr2PcI/view?usp=sharing"> Android</a>
             </span>
           </div>
         </span>
